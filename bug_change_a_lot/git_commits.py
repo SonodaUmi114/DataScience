027cee5c478d97a9d0f86635ca31bf1e6bf3f9c7
Dinggggg <48284002+Dinggggg@users.noreply.github.com>
2020-04-30 15:07:55 +0800

    补充header
    未完成

--- bug_change_a_lot/git_commits.py
+++ bug_change_a_lot/git_commits.py
@@ -1,3 +1,29 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Group1 members information:
+Name, ID, E-mail
+Cao Yanfei    320180939561  caoyf18@lzu.edu.cn
+Cao Yuxuan    320180939571  caoyx2018@lzu.edu.cn
+Ding Junwei   320180939671  dingjw18@lzu.edu.cn
+Gao Shan      320180939740  shgao18@lzu.edu.cn
+Liu Zheng     320180940101  liuzheng2018@lzu.edu.cn
+Qiu Hanqiang  320180940181  479845114@qq.com
+Song Xiujie   320180940211  songxj2018@lzu.edu.cn
+Zhang Zexin   320180940590  zhangzexin18@lzu.edu.cn
+"""
+
+"""
+
+"""
+
+__copyright__ = 'T1,Lanzhou University,2020'
+__license__ = 'GPLV2 or later'
+__version__ = 0.2
+__author__ = ['Hanqiang Qiu','Yanfei Cao','Zheng Liu','Xiujie Song','Yuxuan Cao','Shan Gao','Zexin Zhang','Junwei Ding']
+__email__ = ['479845114@qq.com','caoyf18@lzu.edu.cn','liuzheng2018@lzu.edu.cn','songxj@lzu.edu.cn','caoyx2018@lzu.edu.cn','shgao18@lzu.edu.cn','zhangzexin18@lzu.edu.cn','dingjw18@lzu.edu.cn']
+__status__ = 'done'
+
 import re
 import csv
 from subprocess import Popen, PIPE
